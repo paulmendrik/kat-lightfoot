04f8bf1836d9917b1a5124802e8e4ad538226c61
Paul Mendrik <paul@imwd.dev>
2026-03-19 15:16:45 +0100

    pagination update

--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -1,7 +1,7 @@
 const Pages = [
     { name: "Home", url: "/", key: '1' },
     { name: "About", url: "/about", key: '2' },
-    { name: "Paintings", url: "/paintings/1", key: '3' },
+    { name: "Paintings", url: "/paintings/page/1", key: '3' },
     { name: "Canvas Prints", url: "/canvas", key: '4' },
     { name: "Giclee Prints", url: "/giclee", key: '5' },
     { name: "Exhibitions", url: "/exhibitions", key: '6' },
